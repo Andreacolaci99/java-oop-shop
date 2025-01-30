fee65f04854f98ef50f89f72df073b4689a4e74e
Andreacolaci99 <colaciandrea99@gmail.com>
2025-01-30 18:21:33 +0100

    feat : First Commit

---
+++ Prodotto.java
@@ -0,0 +1,12 @@
+public class Prodotto {
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public int prezzo;
+    public int iva;
+
+    public Prodotto (){
+
+    }
+    
+}
